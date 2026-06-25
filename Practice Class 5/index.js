51085330f9a1934938920dd458868c4ada74a75a
manuelhs <40388035+quantumcloudservices@users.noreply.github.com>
2026-06-25 18:18:15 -0500

    class 5 js

---
+++ Practice Class 5/index.js
@@ -0,0 +1,32 @@
+//TYPE OF FILES
+//Primitive 
+//Numeric - 200, 300, 5, 44.9 
+//Booleans - true or false 
+//String - 'Hey', "Hello", `Hello World` 
+//Null -  intentional lack of an object
+//Undefined - doesn't currently have a value
+//Symbol 
+//BigInt 
+//Complex/Reference
+    //Object
+        //array
+        //object
+        //function
+
+var myName = 'Manuel';
+var myName2 = myName;
+
+myName2 = myName + 'o';
+
+console.log(myName)
+
+let x = 5;
+
+const isBrazilGoingToWinWorldCup = false;
+
+let coolestStudentInClass = Jon;
+
+let coolest2 = Jon;
+
+let y = [1,2,3,4];
+
