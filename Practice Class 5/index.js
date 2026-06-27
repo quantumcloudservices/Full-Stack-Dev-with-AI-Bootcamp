70feb3e11790dfb869f9a4ac13056c4ad3ab90ca
manuelhs <40388035+quantumcloudservices@users.noreply.github.com>
2026-06-27 17:01:19 +0000

    js updates

--- Practice Class 5/index.js
+++ Practice Class 5/index.js
@@ -20,13 +20,42 @@ myName2 = myName + 'o';
 
 console.log(myName)
 
-let x = 5;
-
 const isBrazilGoingToWinWorldCup = false;
 
-let coolestStudentInClass = Jon;
-
-let coolest2 = Jon;
-
 let y = [1,2,3,4];
+
+function happyBirthday(name, age){
+    console.log(`Happy birthday ${name}! You are now ${age}`);
+}
+
+happyBirthday('Manuel', 20);
+
+function addNumbers(num1, num2){
+    return num1 + num2;}
+console.log(addNumbers(5, 10));
+
+function subtractNumbers(num1, num2){
+    return num1 - num2;
+} 
+console.log(subtractNumbers(10, 5));
+
+function multiplyNumbers(num1, num2){
+    return num1 * num2;
+}
+console.log(multiplyNumbers(5, 10));
+
+function divideNumbers(num1, num2){
+    return num1 / num2;
+}
+console.log(divideNumbers(10, 5));
+
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isEven(4));
+console.log(isEven(5));
 
