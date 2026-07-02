04d07f3cabf8803b14d20cc5b5c768c015c70be2
manuelhs <40388035+quantumcloudservices@users.noreply.github.com>
2026-07-02 16:56:11 -0500

    class 7

---
+++ Practice Class 7/script.js
@@ -0,0 +1,15 @@
+let isHouseOnFire = true;
+
+if(isHouseOnFire){
+    console.log('Call 911');
+}else{
+    console.log('Get out of here you noob');
+}
+
+if(age === 18){
+    console.log('let them in');
+} else if(age === 17){
+    console.log('You got two years');
+}else{
+    console.log('NO');
+}
