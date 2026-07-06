58aa265bca7ea0e158a8fc9117aac86ccc10d789
manuelhs <manuelhs23@hotmail.com>
2026-07-06 02:12:21 -0500

    multip 2

--- Simple Calculator/script.js
+++ Simple Calculator/script.js
@@ -1,22 +1,23 @@
- let currentInput = '';
-        
-        function appendToDisplay(value) {
-            currentInput += value;
-            document.getElementById('display').value = currentInput;
-        }
-        
-        function clearDisplay() {
-            currentInput = '';
-            document.getElementById('display').value = '';
-        }
-        
-        function calculate() {
-            try {
-                let result = eval(currentInput);
-                document.getElementById('display').value = result;
-                currentInput = result.toString();
-            } catch (error) {
-                document.getElementById('display').value = 'Error';
-                currentInput = '';
-            }
-        }
+// variable to store my current operation
+let currentInput = '';
+// function to add number or operator to the current operation
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById('display').value = currentInput;
+}     
+// Clearing current operation
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
+ // Calculating current operation
+function calculate() {
+    try {
+        let result = eval(currentInput);
+        document.getElementById('display').value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        currentInput = '';
+    }
+ }
