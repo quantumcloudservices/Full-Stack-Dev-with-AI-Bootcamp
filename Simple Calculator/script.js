8831d61ff5219e0d06403b9fab5a4b507878e36e
manuelhs <manuelhs23@hotmail.com>
2026-07-06 02:36:39 -0500

    including innertext

--- Simple Calculator/script.js
+++ Simple Calculator/script.js
@@ -3,21 +3,21 @@ let currentInput = '';
 // function to add number or operator to the current operation
 function appendToDisplay(value) {
     currentInput += value;
-    document.getElementById('display').value = currentInput;
+    document.getElementById('display').innerText = currentInput;
 }     
 // Clearing current operation
 function clearDisplay() {
     currentInput = '';
-    document.getElementById('display').value = '';
+    document.getElementById('display').innerText = '0';
 }
  // Calculating current operation
 function calculate() {
     try {
         let result = eval(currentInput);
-        document.getElementById('display').value = result;
+        document.getElementById('display').innerText = result;
         currentInput = result.toString();
     } catch (error) {
-        document.getElementById('display').value = 'Error';
+        document.getElementById('display').innerText = 'Invalid Operator';
         currentInput = '';
     }
  }
